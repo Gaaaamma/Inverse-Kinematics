c7e37a8f773ba611d025836a8df3661ddac4f964
Gaaaamma <ytching552022@gmail.com>
2022-05-02 18:10:45 +0800

    hw3 init

---
+++ src/kinematics.cpp
@@ -0,0 +1,78 @@
+#include "kinematics.h"
+
+#include <algorithm>
+
+#include "utils.h"
+void forwardKinematics(const Posture& posture, Bone* bone) {
+  // TODO (FK)
+  // Same as HW2, but have some minor change
+  // Hint:
+  //   1. If you don't use `axis` in this function, you can copy-paste your code
+  // Note:
+  //   1. bone.axis becomes quaternion instead of vector3f
+}
+
+Eigen::VectorXf leastSquareSolver(const Eigen::Matrix3Xf& jacobian, const Eigen::Vector3f& target) {
+  // TODO (find x which min(| jacobian * x - target |))
+  // Hint:
+  //   1. Linear algebra - least squares solution
+  //   2. https://en.wikipedia.org/wiki/Moore%E2%80%93Penrose_inverse#Construction
+  // Note:
+  //   1. SVD or other pseudo-inverse method is useful
+  //   2. Some of them have some limitation, if you use that method you should check it.
+  Eigen::VectorXf solution(jacobian.cols());
+  solution.setZero();
+  return solution;
+}
+
+void inverseKinematics(const Eigen::Vector3f& target, Bone* start, Bone* end, Posture& posture) {
+  constexpr int maxIterations = 10000;
+  constexpr float epsilon = 1E-3f;
+  constexpr float step = 0.1f;
+  // Since bone stores in bones[i] that i == bone->idx, we can use bone - bone->idx to find bones[0] which is root.
+  Bone* root = start - start->idx;
+  std::vector<Bone*> boneList;
+  // TODO
+  // Hint:
+  //   1. Traverse from end to start is easier than start to end (since there is only 1 parent)
+  //   2. If start bone is not reachable from end. Go to root first.
+  // Note:
+  //   1. Both start and end should be in the list
+
+  // Write your code here.
+
+  size_t boneNum = boneList.size();
+  Eigen::Matrix3Xf jacobian(3, 3 * boneNum);
+  jacobian.setZero();
+
+  for (int i = 0; i < maxIterations; ++i) {
+    forwardKinematics(posture, root);
+    // TODO (compute jacobian)
+    //   1. Compute jacobian columns
+    //   2. Compute dTheta
+    // Hint:
+    //   1. You should not put rotation in jacobian if it doesn't have that DoF.
+    //   2. jacobian.col(/* some column index */) = /* jacobian column */
+    //   3. Call leastSquareSolver to compute dTheta
+
+    // Write your code here.
+
+    for (size_t j = 0; j < boneNum; j++) {
+      const auto& bone = *boneList[j];
+      // TODO (update rotation)
+      //   1. Update posture's eulerAngle using deltaTheta
+      // Hint:
+      //   1. Use posture.eulerAngle to get posture's eulerAngle
+      //   2. All angles are in radians.
+      //   3. You can ignore rotation limit of the bone.
+      // Bonus:
+      //   1. You cannot ignore rotation limit of the bone.
+
+      // Write your code here.
+
+      posture.rotations[bone.idx] = Eigen::AngleAxisf(posture.eulerAngle[bone.idx][2], Eigen::Vector3f::UnitZ()) *
+                                    Eigen::AngleAxisf(posture.eulerAngle[bone.idx][1], Eigen::Vector3f::UnitY()) *
+                                    Eigen::AngleAxisf(posture.eulerAngle[bone.idx][0], Eigen::Vector3f::UnitX());
+    }
+  }
+}
